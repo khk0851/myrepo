8469985559a736880ee2c4ad185604e930c2dde6
hyuk <khk0851@naver.com>
2016-04-06 21:32:02 +0900

    pizza homework

--- src/com/j1/w6/Pizza.java
+++ src/com/j1/w6/Pizza.java
@@ -2,16 +2,19 @@ package com.j1.w6;
   
   abstract public class Pizza{
   String name;
+  public String getName(){ /*피자의 이름을 가져오는 함수인 겟네임을 작성*/
+    return name;
+  }
   public void prepare(){
-    System.out.println("preparing"+name);
+    System.out.println("preparing "+name);
   }
   public void bake(){
-    System.out.println("baking"+name);
+    System.out.println("baking "+name);
   }
   public void cut(){
-    System.out.println("cutting"+name);
+    System.out.println("cutting "+name);
   }
   public void box(){
-    System.out.println("boxing"+name);
+    System.out.println("boxing "+name);
   }
 }
