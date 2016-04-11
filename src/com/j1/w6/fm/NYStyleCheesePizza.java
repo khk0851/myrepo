83f1534c16dd2b44b10a63800b6c3558edc37b87
hyuk <khk0851@naver.com>
2016-04-12 00:10:35 +0900

    Pizza FM!

---
+++ src/com/j1/w6/fm/NYStyleCheesePizza.java
@@ -0,0 +1,7 @@
+package com.j1.w6.fm;
+
+public class NYStyleCheesePizza extends Pizza {
+  public NYStyleCheesePizza() {
+    name="NY Style Cheese Pizza";
+  }
+}
---
+++ src/com/j1/w6/fm/NYStyleVeggiePizza.java
@@ -0,0 +1,7 @@
+package com.j1.w6.fm;
+
+public class NYStyleVeggiePizza extends Pizza {
+  public NYStyleVeggiePizza() {
+    name="NY Style Veggie Pizza";
+  }
+}
